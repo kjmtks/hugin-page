2080c8f9b0af399c561ee8170b272215de80e4da
Takeshi Kojima <kjmtks@users.noreply.github.com>
2021-06-09 13:58:39 +0900

    Create environments-for-development.sh

---
+++ res/environments-for-development.sh
@@ -0,0 +1,17 @@
+export APP_URL="http://your-host-name"
+export APP_SECRET_KEY="your-secret-key(need-32-characters)"
+export POSTGRES_USER="your-db-user-for-hugin"
+export POSTGRES_PASSWORD="your-db-user-password-for-hugin"
+
+export LANG=ja_JP.UTF-8
+export TZ=Asia/Tokyo
+export POSTGRES_DB=hugin-development
+export APP_DATA_PATH="$HOME/hugin-app-data-dev"
+export APP_NAME=Hugin
+export APP_DESCRIPTION=""      
+export ASPNETCORE_ENVIRONMENT=Development
+export ASPNETCORE_URLS="http://+:8080"
+export DB_CONNECTION_STR="Host=localhost; Database=$POSTGRES_DB; Port=5432; Username=$POSTGRES_USER; Password=$POSTGRES_PASSWORD"
+export PATH_TO_PROTECTION_KEY="$HOME/hugin-data-protection-dev"
+export ASPNETCORE_Kestrel__Certificates__Default__Path=""
+export ASPNETCORE_Kestrel__Certificates__Default__Password=""
